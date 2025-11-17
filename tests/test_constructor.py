35313f963d5f67eb42907821b6039bbb57d3dd18
Olga Sarbaeva <olga.sarbaeva2018@mail.ru>
2025-11-17 21:32:03 +0500

    my_3_commit_for_5_sprint

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,6 +1,5 @@
 from locators import MainPageLocators
 from urls import URLS
-from constants import SectionTitles
 from helpers.helpers import Helpers
 
 
@@ -10,18 +9,10 @@ class TestBurgerConstructorNavigation:
         """Проверка перехода к разделу булок"""
         driver.get(URLS.MAIN_PAGE)
         
-        # Переходим к соусам, затем возвращаемся к булкам
         driver.find_element(*MainPageLocators.SAUCES_SECTION_BTN).click()
         driver.find_element*(MainPageLocators.BUNS_SECTION_BTN).click()
         
-        # Проверяем, что секция булок активна
         assert Helpers._check_section_is_active(driver, MainPageLocators.BUNS_SECTION_BTN)
-        
-        buns_header = driver.find_element(*MainPageLocators.BUNS_HEADER)
-        buns_section = driver.find_element(*MainPageLocators.BUNS_SECTION)
-        
-        assert buns_header.text == SectionTitles.BUNS
-        assert buns_section.is_displayed()
 
     def test_switch_to_sauces_section(self, driver):
         """Проверка перехода к разделу соусов"""
@@ -29,26 +20,12 @@ class TestBurgerConstructorNavigation:
         
         driver.find_element(*MainPageLocators.SAUCES_SECTION_BTN).click()
         
-        # Проверяем, что секция соусов активна
         assert Helpers._check_section_is_active(driver, MainPageLocators.SAUCES_SECTION_BTN)
         
-        sauces_header = driver.find_element(*MainPageLocators.SAUCES_HEADER)
-        sauces_section = driver.find_element(*MainPageLocators.SAUCES_SECTION)
-        
-        assert sauces_header.text == SectionTitles.SAUCES
-        assert sauces_section.is_displayed()
-
     def test_switch_to_toppings_section(self, driver):
         """Проверка перехода к разделу начинок"""
         driver.get(URLS.MAIN_PAGE)
         
         driver.find_element(*MainPageLocators.TOPPINGS_SECTION_BTN).click()
         
-        # Проверяем, что секция начинок активна
         assert Helpers._check_section_is_active(driver, MainPageLocators.TOPPINGS_SECTION_BTN)
-        
-        toppings_header = driver.find_element(*MainPageLocators.TOPPINGS_HEADER)
-        toppings_section = driver.find_element(*MainPageLocators.TOPPINGS_SECTION)
-        
-        assert toppings_header.text == SectionTitles.TOPPINGS
-        assert toppings_section.is_displayed()
